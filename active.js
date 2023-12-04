787472337bd36dfba8b66992dc8c699c65a4b61a
valencia.judemikaell <valencia.judemikaell@gmail.com>
2023-12-04 18:26:19 +0800

    Add capstone1 folder

---
+++ active.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      // Remove the "active" class from all links
+      navLinks.forEach(function (otherLink) {
+        otherLink.classList.remove("active");
+      });
+
+      // Add the "active" class to the clicked link
+      link.classList.add("active");
+    });
+  });
+});
